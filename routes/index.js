52f47c76542f48e9adae5a7197a3f3ea64651205
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-12-16 16:41:17 +0000

    implemented signup and login page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const {get_landing, submit_lead, show_leads,show_lead,show_edit_lead,edit_lead,delete_lead,delete_lead_json} = require('../controllers/landing');
-const {show_signup,show_login} = require('../controllers/users')
+const {show_signup,show_login,signup,login,logout} = require('../controllers/users')
 
 router.get('/login', show_login);
 router.get('/signup', show_signup);
+router.post('/login', login);
+router.post('/signup', signup);
+router.get('/logout', logout);
+router.post('/logout', logout);
 
 /* GET home page. */
 router.get('/', get_landing );
