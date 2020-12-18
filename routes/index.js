6201b33a5c085e0028a221ad87b4d398848d8eb9
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-12-18 14:34:16 +0000

    Add one route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const {get_landing, submit_lead, show_leads,show_lead,show_edit_lead,edit_lead,delete_lead,delete_lead_json} = require('../controllers/landing');
 const {show_signup,show_login,signup,login,logout} = require('../controllers/users')
+const { isLoggedin} = require('../middleware/hasAuth')
 
 router.get('/login', show_login);
 router.get('/signup', show_signup);
@@ -13,7 +14,7 @@ router.post('/logout', logout);
 /* GET home page. */
 router.get('/', get_landing );
 router.post('/', submit_lead);
-router.get('/leads', show_leads);
+router.get('/leads',isLoggedin, show_leads);
 router.get('/lead/:lead_id', show_lead);
 router.get('/lead/:lead_id/edit', show_edit_lead);
 router.post('/lead/:lead_id/edit', edit_lead);
